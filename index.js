827e3227afd2762f5e23818f4b982fb1013e9727
umsungjun <umseongjun@naver.com>
2024-09-09 21:11:56 +0900

    Feat: README 티스토리 연동

---
+++ index.js
@@ -0,0 +1,41 @@
+import { writeFileSync } from "node:fs";
+import Parser from "rss-parser";
+
+/**
+ * README.MD
+ */
+
+let text = `# Hi there 👋
+
+## 이런 환경에 익숙해요✍🏼
+
+## 📕 Latest Blog Posts
+
+`;
+
+// rss-parser 생성
+const parser = new Parser({
+  headers: {
+    Accept: "application/rss+xml, application/xml, text/xml; q=0.1",
+  },
+});
+
+(async () => {
+  // 피드 목록
+  const feed = await parser.parseURL(
+    "https://developer-sungjun.tistory.com/rss"
+  );
+
+  // 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
+  for (let i = 0; i < 5; i++) {
+    const { title, link } = feed.items[i];
+    text += `<a href=${link}>${title}</a></br>`;
+  }
+
+  // README.md 파일 작성
+  writeFileSync("README.md", text, "utf8", (e) => {
+    console.log(e);
+  });
+
+  console.log("업데이트 완료");
+})();
